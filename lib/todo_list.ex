1d885977b5e081955291e756f1fe577d83cd64a4
Ryota Kinukawa <pojiro.jp@gmail.com>
2021-10-14 20:52:49 +0900

    4.2.4 end

--- lib/todo_list.ex
+++ lib/todo_list.ex
@@ -1,7 +1,9 @@
 defmodule TodoList do
   defstruct auto_id: 1, entries: %{}
 
-  def new(), do: %TodoList{}
+  def new(entries \\ []) do
+    Enum.reduce(entries, %TodoList{}, fn entry, acc -> add_entry(acc, entry) end)
+  end
 
   def add_entry(todo_list, entry) do
     entry = Map.put(entry, :id, todo_list.auto_id)
