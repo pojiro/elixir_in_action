5215f3532da5ace3eaba793398cc16977fd027bb
Ryota Kinukawa <pojiro.jp@gmail.com>
2021-11-18 21:24:28 +0900

    Use Process.register

--- lib/todo_server.ex
+++ lib/todo_server.ex
@@ -1,14 +1,15 @@
 defmodule TodoServer do
   def start() do
-    spawn(fn -> loop(TodoList.new()) end)
+    pid = spawn(fn -> loop(TodoList.new()) end)
+    Process.register(pid, :todo_server)
   end
 
-  def add_entry(todo_server, new_entry) do
-    send(todo_server, {:add_entry, new_entry})
+  def add_entry(new_entry) do
+    send(:todo_server, {:add_entry, new_entry})
   end
 
-  def entries(todo_server, date) do
-    send(todo_server, {:entries, self(), date})
+  def entries(date) do
+    send(:todo_server, {:entries, self(), date})
 
     receive do
       {:todo_entries, entries} -> entries
@@ -17,12 +18,12 @@ defmodule TodoServer do
     end
   end
 
-  def update_entry(todo_server, entry_id, updater_fun) do
-    send(todo_server, {:update_entry, entry_id, updater_fun})
+  def update_entry(entry_id, updater_fun) do
+    send(:todo_server, {:update_entry, entry_id, updater_fun})
   end
 
-  def delete_entry(todo_server, entry_id) do
-    send(todo_server, {:delete_entry, entry_id})
+  def delete_entry(entry_id) do
+    send(:todo_server, {:delete_entry, entry_id})
   end
 
   defp loop(todo_list) do
